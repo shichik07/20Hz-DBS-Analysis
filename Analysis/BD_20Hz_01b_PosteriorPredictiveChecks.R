0fc9d71e1b82e68583f5b7cb40ffe9f5c7d794ef
Julius Kricheldorff <julius@kricheldorff.de>
2024-08-22 13:41:39 +0200

    accuracy Plots now in %

--- Analysis/BD_20Hz_01b_PosteriorPredictiveChecks.R
+++ Analysis/BD_20Hz_01b_PosteriorPredictiveChecks.R
@@ -96,7 +96,7 @@ rm(a, fit_shifted_log_GNG)
 load(file = "D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Data/Modelle/log_reg_GNG.rda")
 
 # get predictions 
-acc_GNG_pred <- brms::posterior_epred(fit_log_flanker2)
+acc_GNG_pred <- brms::posterior_epred(fit_log_flanker2)*100
 
 # set up the figure
 png(file="D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Figures/Posterior_Checks/RT_GNG_acc_posterior_pred.png",
@@ -113,11 +113,11 @@ mean_130_Go <-mean(GoNoGo$Error[GoNoGo$Contrast_F == "S130Hz_Go"])
 
 # Plot 130 Figures
 hist(S130Hz_NoGo_Go, main = "NoGo (Go):  130Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_130_NoGo_Go, col = "red", lwd = 3)
+abline(v = mean_130_NoGo_Go*100, col = "red", lwd = 3)
 hist(S130Hz_NoGo_Stop, main = "NoGo (Stop):  130Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_130_NoGo_Stop, col = "red", lwd = 3)
+abline(v = mean_130_NoGo_Stop*100, col = "red", lwd = 3)
 hist(S130Hz_Go, main = "Go:  130Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_130_Go, col = "red", lwd = 3)
+abline(v = mean_130_Go*100, col = "red", lwd = 3)
 
 # get data for 20Hz
 S20Hz_NoGo_Go <- rowMeans(acc_GNG_pred[,GoNoGo$Contrast_F == "S20Hz_NoGo_Go"])
@@ -129,11 +129,11 @@ mean_20_Go <-mean(GoNoGo$Error[GoNoGo$Contrast_F == "S20Hz_Go"])
 
 # Plot 20 Figures
 hist(S20Hz_NoGo_Go, main = "NoGo (Go):  20Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_20_NoGo_Go, col = "red", lwd = 3)
+abline(v = mean_20_NoGo_Go*100, col = "red", lwd = 3)
 hist(S20Hz_NoGo_Stop, main = "NoGo (Stop):  20Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_20_NoGo_Stop, col = "red", lwd = 3)
+abline(v = mean_20_NoGo_Stop*100, col = "red", lwd = 3)
 hist(S20Hz_Go, main = "Go:  20Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_20_Go, col = "red", lwd = 3)
+abline(v = mean_20_Go*100, col = "red", lwd = 3)
 
 # get data for OFF
 SOFF_NoGo_Go <- rowMeans(acc_GNG_pred[,GoNoGo$Contrast_F == "SOFF_NoGo_Go"])
@@ -145,11 +145,11 @@ mean_OFF_Go <-mean(GoNoGo$Error[GoNoGo$Contrast_F == "SOFF_Go"])
 
 # Plot OFF Figures
 hist(SOFF_NoGo_Go, main = "NoGo (Go):  OFF Stimulation", xlab = c("Error in %"))
-abline(v = mean_OFF_NoGo_Go, col = "red", lwd = 3)
+abline(v = mean_OFF_NoGo_Go*100, col = "red", lwd = 3)
 hist(SOFF_NoGo_Stop, main = "NoGo (Stop):  OFF Stimulation", xlab = c("Error in %"))
-abline(v = mean_OFF_NoGo_Stop, col = "red", lwd = 3)
+abline(v = mean_OFF_NoGo_Stop*100, col = "red", lwd = 3)
 hist(SOFF_Go, main = "Go:  OFF Stimulation", xlab = c("Error in %"))
-abline(v = mean_OFF_Go, col = "red", lwd = 3)
+abline(v = mean_OFF_Go*100, col = "red", lwd = 3)
 
 # save 
 dev.off()
@@ -214,7 +214,7 @@ rm(RT_SRT_pred, fit_shifted_log_SRT)
 load(file = "D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Data/Modelle/log_reg_SRT.rda")
 
 # first calculate the posterior predictions
-acc_SRT_pred <- brms::posterior_epred(fit_logReg_SRT)
+acc_SRT_pred <- brms::posterior_epred(fit_logReg_SRT)*100
 
 # summarize over conditions
 png(file="D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Figures/Posterior_Checks/acc_SRT_posterior_pred.png",
@@ -231,10 +231,10 @@ mean_20_130 <- mean(SimpleRT$Error[SimpleRT$StimCon == "S20Hz"]) - mean(SimpleRT
 mean_20_OFF <- mean(SimpleRT$Error[SimpleRT$StimCon == "S20Hz"]) - mean(SimpleRT$Error[SimpleRT$StimCon == "SOFF"])
 
 # Plot Figures
-hist(pred_20_130, main = "RT 20Hz versus 130Hz", xlab = c("difference in ms"))
-abline(v = mean_20_130, col = "red", lwd = 3)
-hist(pred_20_OFF, main = "RT 20Hz versus OFF", xlab = c("difference in ms"))
-abline(v = mean_20_OFF, col = "red", lwd = 3)
+hist(pred_20_130, main = "RT 20Hz versus 130Hz", xlab = c("difference in %"))
+abline(v = mean_20_130*100, col = "red", lwd = 3)
+hist(pred_20_OFF, main = "RT 20Hz versus OFF", xlab = c("difference in %"))
+abline(v = mean_20_OFF*100, col = "red", lwd = 3)
 
 #save
 dev.off()
@@ -337,7 +337,7 @@ rm(RT_FLT_pred, fit_shifted_log_FLT)
 load(file = "D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Data/Modelle/log_reg_FLT.rda")
 
 # calculate predictions
-acc_FLT_pred <- brms::posterior_epred(fit_log_FLT)
+acc_FLT_pred <- brms::posterior_epred(fit_log_FLT)*100
 
 # summarize over conditions
 png(file="D:/Data/Dropbox/PhD_Thesis/UniOL/Julius/20Hz-DBS-Analysis/Figures/Posterior_Checks/acc_FLT_posterior_pred.png",
@@ -351,7 +351,7 @@ mean_130Hz_Stroop <- mean(FLTRT$Error[FLTRT$Contrast_F == "S130Hz_incongruent"])
 
 # Plot 130 Figures
 hist(S130Hz_Incongruent - S130Hz_Congruent, main = "Stroop:  130Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_130Hz_Stroop, col = "red", lwd = 3)
+abline(v = mean_130Hz_Stroop*100, col = "red", lwd = 3)
 
 
 # get data for 20Hz
@@ -361,7 +361,7 @@ mean_20Hz_Stroop <- mean(FLTRT$Error[FLTRT$Contrast_F == "S20Hz_incongruent"]) -
 
 # Plot 20 Figures
 hist(S20Hz_Incongruent - S20Hz_Congruent, main = "Stroop:  20Hz Stimulation", xlab = c("Error in %"))
-abline(v = mean_20Hz_Stroop, col = "red", lwd = 3)
+abline(v = mean_20Hz_Stroop*100, col = "red", lwd = 3)
 
 # get data for OFF
 SOFF_Congruent <- rowMeans(acc_FLT_pred[,FLTRT$Contrast_F == "SOFF_congruent"])
@@ -370,7 +370,7 @@ mean_OFF_Stroop <- mean(FLTRT$Error[FLTRT$Contrast_F == "SOFF_incongruent"]) - m
 
 # Plot 20 Figures
 hist(SOFF_Incongruent - SOFF_Congruent, main = "Stroop:  OFF Stimulation", xlab = c("Error in %"))
-abline(v = mean_OFF_Stroop, col = "red", lwd = 3)
+abline(v = mean_OFF_Stroop*100, col = "red", lwd = 3)
 
 # Plot the difference between 20Hz overall and OFF and 130Hz
 S130Hz_Overall <- rowMeans(acc_FLT_pred[,FLTRT$Stim_verb == "130Hz"])
@@ -381,11 +381,11 @@ SOFF_Overall <- rowMeans(acc_FLT_pred[,FLTRT$Stim_verb == "OFF"])
 mean_OFF_Overall <- mean(FLTRT$Error[FLTRT$Stim_verb == "OFF"])
 
 # Plot 20 vs 130 Figures
-hist(S20Hz_Overall - S130Hz_Overall, main = "20Hz vs 130Hz Overall", xlab = c("Error in %"))
-abline(v = mean_20Hz_Overall-mean_130Hz_Overall, col = "red", lwd = 3)
+hist(S20Hz_Overall - S130Hz_Overall, main = "20Hz versus 130Hz Overall", xlab = c("Error difference in %"))
+abline(v = (mean_20Hz_Overall-mean_130Hz_Overall)*100, col = "red", lwd = 3)
 # Plot 20 vs OFF Figures
-hist(S20Hz_Overall - SOFF_Overall, main = "20Hz vs OFF Overall", xlab = c("Error in %"))
-abline(v = mean_20Hz_Overall-mean_OFF_Overall, col = "red", lwd = 3)
+hist(S20Hz_Overall - SOFF_Overall, main = "20Hz versus OFF Overall", xlab = c("Error difference in %"))
+abline(v = (mean_20Hz_Overall-mean_OFF_Overall)*100, col = "red", lwd = 3)
 
 # save 
 dev.off()
